2837d8fdf67e76d0900dc99862135f5835a046d2
Katherine Wang <katherine.wang429@gmail.com>
2019-12-10 15:07:32 -0800

    Copy over dependencies and server files

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const getAllListings = require('../db').getAllListings;
+const getOneListing = require('../db').getOneListing;
+
+const app = express();
+
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`Connected to port ${port}`));
+
+app.get('/listings', (req, res) => {
+  getAllListings()
+    .then((docs) => res.status(200).send(docs));
+})
+
+app.get('/listings/:id', ({ params }, res) => {
+  getOneListing(params.id)
+    .then((docs) => res.status(200).send(docs));
+})
